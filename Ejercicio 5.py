a5fac5e3cdeed5979bb570b9f6443fb7e6303ea3
abohera <abohera@isntitutmvm.cat>
2021-01-21 12:16:10 +0100

    Initial commit

--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -2,9 +2,10 @@ def main():
     numero=0
     for numero in range(0,11):
         print(("\nTabla del %d") % (numero))
+        print("------------")
         for i in range(0,11):
             total=i*numero
-            print(("%d*%d= %d")%(numero,i,total))
+            print(("%d * %d = %d")%(numero,i,total))
 
 if __name__ == "__main__":
     main()
