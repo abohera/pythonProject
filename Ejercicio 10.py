d6e76cf20d528cd8b4d814d2986e7b49c1074e70
abohera <abohera@isntitutmvm.cat>
2021-01-25 10:29:40 +0100

    Añadidos Comentarios al código

--- Ejercicio 10.py
+++ Ejercicio 10.py
@@ -1,11 +1,14 @@
 def main():
-    segundos= int(input("Introduce el total de segundos: "))
-    dias= segundos // (24 * 60 * 60)
-    segundos= segundos % (24 * 60 * 60)
-    horas= segundos // (60 * 60)
+    # El usuario introducira los segundos por teclado
+    segundos = int(input("Introduce el total de segundos: "))
+    # Se haran la operaciones correspondientes y mostrará el resultado por pantalla
+    dias = segundos // (24 * 60 * 60)
+    segundos = segundos % (24 * 60 * 60)
+    horas = segundos // (60 * 60)
     segundos = segundos % (60 * 60)
     minutos = segundos // 60
     segundos = segundos % 60
+    # Mostrará los segundos introducidos por teclado en formato: Días/Horas/Minutos/Segundos
     print("{} Día/s | {} Hora/s | {} Minuto/s | {} Segundo/s".format(dias, horas, minutos, segundos))
 
 if __name__ == "__main__":
--- Ejercicio 5.py
+++ Ejercicio 5.py
@@ -1,14 +1,14 @@
 def main():
-# Numero es la variable que se incrementara del 0 al 10
-    numero=0
-# La primera estructura de repetición es un for con un rango de la posición 0 a la 11, con lo cual mostrara 10 veces el Titulo "Tabla de multiplicar del 'numero'" en cuestión
-    for numero in range(0,11):
+    # Numero es la variable que se incrementara del 0 al 10
+    numero = 0
+    # La primera estructura de repetición es un for con un rango de la posición 0 a la 11, con lo cual mostrara 10 veces el Titulo "Tabla de multiplicar del 'numero'" en cuestión
+    for numero in range(0, 11):
         print(("\nTabla de multiplicar del %d") % (numero))
         print("------------")
-# La segunda estructura de repetición es un for con el mismo rango que el anterior, pero este se encarga de calcular el resultado de las operaciones y mostrarlas por pantalla
-        for i in range(0,11):
-            total=i*numero
-            print(("%d * %d = %d")%(numero,i,total))
+    # La segunda estructura de repetición es un for con el mismo rango que el anterior, pero este se encarga de calcular el resultado de las operaciones y mostrarlas por pantalla
+        for i in range(0, 11):
+            total = i*numero
+            print(("%d * %d = %d") % (numero, i, total))
 
 if __name__ == "__main__":
     main()
