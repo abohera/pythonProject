67c2b37cec0431b547c4049aed708d5814f032f0
abohera <abohera@isntitutmvm.cat>
2021-01-28 10:09:34 +0100

    Comentarios añadidos

--- Ejercicio 4.py
+++ Ejercicio 4.py
@@ -1,17 +1,19 @@
 N=7
 
 def main():
-    for i in range(1, 8):
-        i += 1
-        for j in range(1, N-1):
-            j += 1
-            print(" ")
-        for k in range(i, 1):
-            k -= 1
-            print(k)
-        for x in range(2, i-1):
-            x += 1
-            print(x)
+    for i in range(N):
+        k = i + 1
+        for j in range(N-i-1):
+            print(" ", end="")
+        for j in range(i+1):
+            print(k, end="")
+            k = k - 1
+        if i>0:
+            for j in range(i-1):
+                print(" ", end="")
+            for j in range(i+1):
+                k = k + 1
+                print(k, end="")
 
 if __name__ == "__main__":
     main()
