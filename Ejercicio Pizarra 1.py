087ed07d5f15f86114aec026c3ec84d6aa54f760
abohera <abohera@isntitutmvm.cat>
2021-02-04 11:45:43 +0100

    Pizarra 1

--- Ejercicio Pizarra 1.py
+++ Ejercicio Pizarra 1.py
@@ -1,8 +1,8 @@
 def main():
-    aulas_dicc = {}
+    aulas_dict = {}
     registros = int(input("¿Cuantos registros queires introducir?: "))
     for i in range(registros):
-        print("\nNúmero de registo %u: " % (i + 1))
+        print("\nRegisto %u: " % (i + 1))
         num = int(input("Introduce el número del aula: "))
         while num < 1 or num > 50:
             num = int(input("Introduce un número del 1 al 50: "))
@@ -11,12 +11,11 @@ def main():
         pc = int(input("Introduce el número de pc's en el aula: "))
         while pc < 1 or pc > 20:
             pc = int(input("Introduce un número del 1 al 20: "))
-        aulas_dicc[i] = {
-            "Aula" : { "Número": num, "Nombre": nom, "IP": ip, "PC": pc}
-        }
+        aulas_dict[i] = num, nom, ip, pc
     print("\n")
-    for x in aulas_dicc.values():
-        print(x)
+    print("Num Nom IP PCs")
+    for x, y in aulas_dict.items():
+        print(x, y)
 
 if __name__ == "__main__":
     main()
